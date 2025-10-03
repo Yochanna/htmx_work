208aa86fad27c4400e29bd6b68c95ec249340421
Jane <razwiedanijane@gmail.com>
2025-10-03 14:50:38 +0200

    Remove old folder django_dynamic_form_project_final_demo (not used anymore)

--- django_dynamic_form_project_final_demo/dynamic_forms/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class DynamicFormsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dynamic_forms'
--- django_dynamic_form_project_final_demo/dynamic_forms/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.dynamic_form_view, name="dynamic-form"),
-    path('load-form/', views.load_form_fields, name="load-form"),
-    path('load-topics/', views.load_topics, name="load-topics"),
-    path('submit-form/', views.submit_form, name="submit-form"),
-]
