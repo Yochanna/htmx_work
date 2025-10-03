b662c0199068ae5926f8fcfacb479ae30c565025
Jane <razwiedanijane@gmail.com>
2025-10-03 14:45:38 +0200

    Remove old yochanna_todo and yochanna_todo_righthere projects

--- yochanna_todo/myproject/yochanna/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("add-task/", views.add_task, name="add_task"),
-    path("mark-done/<int:task_id>/", views.mark_done, name="mark_done"),
-]
